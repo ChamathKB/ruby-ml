eafdc045864c070b87c775391e8d37671374a074
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-12-02 00:11:37 +0530

    update object detection demo script

--- object-detection-demo/object_detection.rb
+++ object-detection-demo/object_detection.rb
@@ -1,26 +1,17 @@
 require "onnxruntime"
 require "mini_magick"
 
-image_path = './data/sample.jpg'
-model_path = './models/ssd_mobilenet_v1_12.onnx' 
+def load_image(image_path)
+  MiniMagick::Image.open(image_path)
+end
 
-img = MiniMagick::Image.open(image_path)
-pixels = img.get_pixels
+def load_model(model_path)
+  OnnxRuntime::Model.new(model_path)
+end
 
-model = OnnxRuntime::Model.new(model_path)
-result = model.predict({"inputs" => [pixels]})
-
-p model.inputs
-p model.outputs
-p model.metadata
-
-p result["num_detections"]
-p result["detection_classes"]
-
-coco_labels = {
-  23 => "bear",
-  88 => "teddy bear"
-}
+def get_image_pixels(image)
+  image.get_pixels
+end
 
 def draw_box(img, label, box)
   width, height = img.dimensions
@@ -31,7 +22,6 @@ def draw_box(img, label, box)
   bottom = (box[2] * height).round + thickness
   right = (box[3] * width).round + thickness
 
-  # draw box
   img.combine_options do |c|
     c.draw "rectangle #{left},#{top} #{right},#{bottom}"
     c.fill "none"
@@ -39,7 +29,6 @@ def draw_box(img, label, box)
     c.strokewidth thickness
   end
 
-  # draw text
   img.combine_options do |c|
     c.draw "text #{left},#{top - 5} \"#{label}\""
     c.fill "red"
@@ -47,13 +36,51 @@ def draw_box(img, label, box)
   end
 end
 
-result["num_detections"].each_with_index do |n, idx|
-  n.to_i.times do |i|
-    label = result["detection_classes"][idx][i].to_i
-    label = coco_labels[label] || label
-    box = result["detection_boxes"][idx][i]
-    draw_box(img, label, box)
+def predict(model, pixels)
+  model.predict({"inputs" => [pixels]})
+end
+
+def label_image(img, result, coco_labels)
+  result["num_detections"].each_with_index do |n, idx|
+    n.to_i.times do |i|
+      label = result["detection_classes"][idx][i].to_i
+      label = coco_labels[label] || label
+      box = result["detection_boxes"][idx][i]
+      draw_box(img, label, box)
+    end
   end
 end
 
-img.write("labeled.jpg")
+def print_model_and_result_info(model, result)
+  puts "Model Inputs: #{model.inputs}"
+  puts "Model Outputs: #{model.outputs}"
+  puts "Model Metadata: #{model.metadata}"
+
+  puts "Number of Detections: #{result["num_detections"]}"
+  puts "Detection Classes: #{result["detection_classes"]}"
+end
+
+def save_labeled_image(img, output_path)
+  img.write(output_path)
+end
+
+# Main program
+
+image_path = './data/sample.jpg'
+model_path = './models/ssd_mobilenet_v1_12.onnx'
+output_path = "labeled.jpg"
+
+img = load_image(image_path)
+pixels = get_image_pixels(img)
+
+model = load_model(model_path)
+result = predict(model, pixels)
+
+coco_labels = {
+  23 => "bear",
+  88 => "teddy bear"
+}
+
+print_model_and_result_info(model, result)
+label_image(img, result, coco_labels)
+save_labeled_image(img, output_path)
